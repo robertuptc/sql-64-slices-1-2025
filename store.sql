bd41d8a021f311e2ba7cda17b1a1dfddc91d1fb1
robertuptc <robertuptc@gmail.com>
2025-04-28 22:41:41 -0400

    stage ii finished

--- store.sql
+++ store.sql
@@ -1,64 +1,75 @@
 DROP TABLE IF EXISTS available_pizzas, available_toppings, customers, deliveries, drivers, order_toppings, orders, stores;
 
-CREATE TABLE available_pizzas(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    cost DECIMAL(5, 2)
-);
-
-CREATE TABLE available_toppings(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    cost_per_pizza DECIMAL(5,2)
-);
-
-CREATE TABLE customers(
-    id SERIAL PRIMARY KEY,
-    street VARCHAR,
-    city VARCHAR(50),
-    zip VARCHAR(10),
-    country VARCHAR(20)
-);
-
-CREATE TABLE deliveries(
-    id SERIAL PRIMARY KEY,
-    driver_id BIGINT,
-    order_id BIGINT,
-    started_delivery TIME,
-    completed_delivery TIME
-);
-
-CREATE TABLE drivers(
-    id SERIAL PRIMARY KEY,
-    store_id BIGINT,
-    full_name VARCHAR(100)
-);
-
-CREATE TABLE order_toppings(
-    id SERIAL PRIMARY KEY,
-    order_id BIGINT,
-    topping_id BIGINT
-);
-
-CREATE TABLE orders(
-    order_id SERIAL PRIMARY KEY,
-    customer_id BIGINT,
-    date DATE,
-    pizza_type VARCHAR(50),
-    store_id INT,
-    toppings VARCHAR
-);
-
 CREATE TABLE stores(
     id SERIAL PRIMARY KEY,
     location VARCHAR
 );
 
+CREATE TABLE available_pizzas(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+        CHECK(name IN('small', 'medium', 'large')),
+    cost DECIMAL(5, 2)
+);
+
+CREATE TABLE available_toppings(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    cost_per_pizza DECIMAL(5,2) NOT NULL
+);
+
+CREATE TABLE customers(
+    id SERIAL PRIMARY KEY,
+    street VARCHAR(200) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    zip VARCHAR(10) NOT NULL,
+    country VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE drivers(
+    id SERIAL PRIMARY KEY,
+    store_id INT NOT NULL,
+    full_name VARCHAR(100) NOT NULL
+        CHECK(full_name ~ '^[A-Z][a-z0-9]*(?:-[A-Z][a-z0-9]*)*(?: [A-Z0-9][a-z0-9]*(?:-[A-Z0-9][a-z0-9]*)*)*$'),
+    FOREIGN KEY (store_id) REFERENCES stores(id)
+);
+
+CREATE TABLE orders(
+    order_id SERIAL PRIMARY KEY,
+    customer_id BIGINT NOT NULL,
+    date DATE NOT NULL,
+    pizza_type INT NOT NULL
+        CHECK (pizza_type IN (1, 2, 3)),
+    store_id INT,
+    toppings VARCHAR, -- UNSURE about this, a many-to-many relationship was created, but the csv file still have the toppings column. hat to do?
+    FOREIGN KEY (store_id) REFERENCES stores(id)
+);
+
+CREATE TABLE order_toppings(
+    id SERIAL PRIMARY KEY,
+    order_id INT,
+    topping_id INT,  
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (topping_id) REFERENCES available_toppings(id)
+);
+
+CREATE TABLE deliveries(
+    id SERIAL PRIMARY KEY,
+    driver_id INT,
+    order_id INT UNIQUE,
+    started_delivery TIME NOT NULL,
+    completed_delivery TIME NOT NULL,
+    FOREIGN KEY (driver_id) REFERENCES drivers(id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
+
+
+\COPY stores FROM './stage-1/data/stores.csv' WITH CSV HEADER;
 \COPY available_pizzas FROM './stage-1/data/available_pizzas.csv' WITH CSV HEADER;
 \COPY available_toppings FROM './stage-1/data/available_toppings.csv' WITH CSV HEADER;
 \COPY customers FROM './stage-1/data/customers.csv' WITH CSV HEADER;
-\COPY deliveries FROM './stage-1/data/deliveries.csv' WITH CSV HEADER;
 \COPY drivers FROM './stage-1/data/drivers.csv' WITH CSV HEADER;
-\COPY order_toppings FROM './stage-1/data/order_toppings.csv' WITH CSV HEADER;
 \COPY orders FROM './stage-1/data/orders.csv' WITH CSV HEADER;
-\COPY stores FROM './stage-1/data/stores.csv' WITH CSV HEADER;
+\COPY order_toppings FROM './stage-1/data/order_toppings.csv' WITH CSV HEADER;
+\COPY deliveries FROM './stage-1/data/deliveries.csv' WITH CSV HEADER;
